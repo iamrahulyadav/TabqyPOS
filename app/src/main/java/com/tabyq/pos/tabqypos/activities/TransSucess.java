d056a6e11dc5d3e18de7e1a575b9b8feff6f43f3
lenovo <rakhi@askonlinesolution.com>
2018-08-01 10:36:06 +0530

    change by Rakhi 01/08/2018 06:43PM

--- app/src/main/java/com/tabyq/pos/tabqypos/activities/TransSucess.java
+++ app/src/main/java/com/tabyq/pos/tabqypos/activities/TransSucess.java
@@ -1,5 +1,6 @@
 package com.tabyq.pos.tabqypos.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +18,9 @@ public class TransSucess extends AppBaseActivity {
     }
 
     public void trans_success_back(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
         finish();
     }
 
